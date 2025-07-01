6063fbb25c0706165ca9f52c136651a8ccd4b6b8
Skelesheit <Skelesheit@gmail.com>
2025-07-01 20:15:12 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <div class="p-6">
+    <h1 class="text-red-500">Tailwind работает?</h1>
+    <p class="bg-green-400">ОК </p>
+    <button class="btn btn-accent mt-4">Кнопка DaisyUI</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
